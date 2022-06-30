b9e8c850db1c27fafde398ba447547cdf6732301
Paulo Lucena <paulo.henriquegvl@gmail.com>
2022-06-30 12:11:16 -0300

    fist commmit

---
+++ CalcularIdade.Console/Entities/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularIdade.Console.Entities
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Person(string name, DateTime birthDate)
+        {
+            Name = name;
+            BirthDate = birthDate;
+        }
+
+        private int CalcularIdade()
+        {
+            DateTime d1 = DateTime.Now;
+            double idade = (d1 - BirthDate).Days / 365;
+
+            if (d1.Month >= BirthDate.Month && d1.Day > BirthDate.Day)
+            {
+                return (int)--idade;
+            }
+            else
+            {
+                return (int)idade;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Olá, {Name}. Sua idade é {CalcularIdade()}";
+        }
+
+    }
+}
---
+++ CalcularIdade.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using CalcularIdade.Console.Entities;
+
+Console.WriteLine("Informe seu nome: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Informe sua data de nascimento: ");
+DateTime birthDay = DateTime.Parse(Console.ReadLine());
+
+Person person = new Person(name, birthDay);
+Console.WriteLine("");
+Console.WriteLine(person.ToString());
